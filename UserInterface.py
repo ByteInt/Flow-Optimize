c5b052b449d7c57ce85c47f2d07e635a6e9f3acc
Page <68625666+ByteInt@users.noreply.github.com>
2022-04-13 21:14:53 +0800

    Add working mode selection

--- UserInterface.py
+++ UserInterface.py
@@ -5,17 +5,24 @@ from tkinter import ttk
 
 def add_apparatus():
     def add_apparatus_ok():
-        device_list.insert(0, device_name.get())
+        device_list.insert(0, device_name.get() + ' working at ' + working_mode.get() + ' mode')
         add_window.destroy()
+
     add_window = Toplevel()
     add_window.geometry('300x200')
     add_window.title('Add Device')
     ok_button = Button(add_window, text="OK", command=add_apparatus_ok, default='active')
     device_name_label = Label(add_window, text='Device name:')
     device_name = Entry(add_window, width=20)
-    device_name_label.pack()
-    device_name.pack(padx=20, pady=20)
-    ok_button.pack(padx=20, pady=20)
+    working_mode_label = Label(add_window, text='Working mode:')
+    working_mode = ttk.Combobox(add_window)
+    working_mode['value'] = ('Fixed', 'Optimize', 'Chained')
+
+    device_name_label.pack(padx=5, pady=5)
+    device_name.pack(padx=10, pady=10)
+    working_mode_label.pack(padx=5, pady=5)
+    working_mode.pack(padx=10, pady=10)
+    ok_button.pack(padx=10, pady=10)
     add_window.mainloop()
 
 
@@ -41,7 +48,7 @@ if __name__ == '__main__':
     # Connection选项卡内容
     add_device = Button(connection_frame, text='Add Device', command=add_apparatus)
     add_device.pack(padx=10, pady=10)
-    device_list = Listbox(connection_frame)
+    device_list = Listbox(connection_frame, width=100, height=20)
     device_list.pack(padx=10, pady=10)
 
     # Method选项卡内容
@@ -49,7 +56,7 @@ if __name__ == '__main__':
     experiment_time = Entry(method_frame)
     sampinterval_label = Label(method_frame, text="Sampling interval(min):")
     sample_interval = Entry(method_frame)
-    samppoints_label = Label(method_frame, text= "Sample points:")
+    samppoints_label = Label(method_frame, text="Sample points:")
     sample_points = Spinbox(method_frame, increment=1)
 
     extime_label.pack(padx=5, pady=5)
@@ -61,6 +68,9 @@ if __name__ == '__main__':
     method_confirm = Button(method_frame, text="Confirm", command=method_confirm)
     method_confirm.pack(padx=5, pady=5)
 
+    #Parameters选项卡内容
+
+    # 加载notebook选项卡组件
     notebook.add(connection_frame, text='Connection')
     notebook.add(method_frame, text='Method')
     notebook.add(parameters_frame, text='Parameters')
@@ -68,4 +78,3 @@ if __name__ == '__main__':
     notebook.pack(padx=10, pady=10, fill=tkinter.BOTH, expand=True)
 
     root.mainloop()
-
