a4c4b7bfbc875060af9b09adfe4be6db87755d43
Page <68625666+ByteInt@users.noreply.github.com>
2022-04-13 15:48:26 +0800

    A simple user interface

--- UserInterface.py
+++ UserInterface.py
@@ -1,5 +1,16 @@
-import tkinter as tk
+from tkinter import *
+
+
+def add_apparatus():
+    device_list.insert(0, 'Device')
+
 
 if __name__ == '__main__':
-    pass
-
+    root = Tk()
+    root.title('Flow Control')
+    root.geometry('800x600')
+    add_device = Button(root, text='Add Device', command=add_apparatus)
+    add_device.pack()
+    device_list = Listbox(root)
+    device_list.pack()
+    root.mainloop()
