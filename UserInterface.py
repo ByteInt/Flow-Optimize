6d63027cf8240ad691f5eff882a59a94a88d5dea
Page <68625666+ByteInt@users.noreply.github.com>
2022-04-13 19:22:23 +0800

    Add choice frames

--- UserInterface.py
+++ UserInterface.py
@@ -1,4 +1,6 @@
+import tkinter
 from tkinter import *
+from tkinter import ttk
 
 
 def add_apparatus():
@@ -17,14 +19,26 @@ def add_apparatus():
     add_window.mainloop()
 
 
-
 if __name__ == '__main__':
     root = Tk()
     root.title('Flow Control')
     root.geometry('800x600')
-    add_device = Button(root, text='Add Device', command=add_apparatus)
-    add_device.pack(padx=20, pady=20)
-    device_list = Listbox(root)
-    device_list.pack(padx=20, pady=20)
+    notebook = ttk.Notebook(root)  # Notebook是选项卡容器
+    connection_frame = Frame(notebook)
+    method_frame = Frame(notebook)
+    parameters_frame = Frame(notebook)
+    optimize_frame = Frame(notebook)
+    # 暂定的四个选项卡
+    add_device = Button(connection_frame, text='Add Device', command=add_apparatus)
+    add_device.pack(padx=10, pady=10)
+    device_list = Listbox(connection_frame)
+    device_list.pack(padx=10, pady=10)
+
+    notebook.add(connection_frame, text='Connection')
+    notebook.add(method_frame, text='Method')
+    notebook.add(parameters_frame, text='Parameters')
+    notebook.add(optimize_frame, text='Optimize')
+    notebook.pack(padx=10, pady=10, fill=tkinter.BOTH, expand=True)
+
     root.mainloop()
 
