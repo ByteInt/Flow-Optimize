5f8da879da00bbd34ec23ddea261f9c0d29a6774
Page <68625666+ByteInt@users.noreply.github.com>
2022-07-13 18:03:42 +0800

    The program is now able to run close-looped virtual experiments

--- Commandline.py
+++ Commandline.py
@@ -2,14 +2,15 @@ import math
 import pathlib
 import time
 from random import random
-
 import pkg_resources
 from summit.domain import *
 from summit.benchmarks import SnarBenchmark, ExperimentalEmulator
 from summit.utils.dataset import DataSet
-from summit.strategies import NelderMead, MultitoSingleObjective, SOBO
+from summit.strategies import NelderMead, MultitoSingleObjective, SOBO, Random
 import csv
 from summit.run import Runner
+
+import Detector
 import Device
 
 
@@ -17,9 +18,6 @@ def initiate():
     global domain, ds, strategy, writer
     # The csv file for experiment data
 
-    # STRATEGY the strategy in pysummit
-
-    #
     domain = Domain()
 
     # area for adding domain
@@ -40,29 +38,23 @@ def initiate():
         name='conti_2',
         description='5',
         bounds=[0,1],
-        is_objective=True
     )
     domain += ContinuousVariable(
         name='yld',
         description='6',
         bounds=[0,100],
-        is_objective=True
+        is_objective=True,
+        maximize=True
     )
     print(domain.variables)
     # Generate title of the csv file
     generate_title()
     # Test data
-    generate_data()
-    # The problem is that, the changes of csv file can only be applied when the program is over.
-    time.sleep(1)
+    # generate_data()
 
     '''transform = MultitoSingleObjective(
         domain, expression = "", maximize = True
     )'''
-
-    strategy = SOBO(domain)
-    data_path = pathlib.Path()
-    ds = DataSet.read_csv(data_path / 'experiment.csv')
 
 
 def generate_title():
@@ -78,6 +70,7 @@ def generate_title():
         print(row_1)
         writer.writerow(row_1)
         writer.writerow(row_2)
+        csvfile.close()
 
 
 def generate_data():
@@ -92,9 +85,47 @@ def generate_data():
             writer.writerow(randrow)
         csvfile.close()
 
+
+def single_experiment(var1, var2, var3):
+    # Setting some equipments to var1, 2 and 3
+    print('New experiment in progress with following parameters:\nvar1:' + var1
+          + '\nvar2:' + var2 + '\nvar3:' + var3)
+    # Waiting time based on experiment settings
+    time.sleep(1)
+    result = Detector.detect()
+    with open('experiment.csv', newline='') as csvfile:
+        lines = csvfile.readlines()
+        last_line = lines[-1]
+        last_entry = last_line.split(',')[0]
+        if last_entry == 'TYPE':
+            index = 0
+        else:
+            index = int(last_entry) + 1
+        csvfile.close()
+    with open('experiment.csv', 'a', newline='') as csvfile:
+        writer = csv.writer(csvfile)
+        next_row = [index, var1, var2, var3, result]
+        writer.writerow(next_row)
+        csvfile.close()
+
+
 if __name__ == '__main__':
     initiate()
-    exp = ExperimentalEmulator(model_name='Model', domain=domain, dataset=ds)
-    exp.train(max_epochs=500, cv_fold=10, test_size=0.2)
-    next_experiments = strategy.suggest_experiments(2)
-    print(str(next_experiments))
+    # pre-experiments: data are given by random
+    strategy = Random(domain)
+    for i in range(10):
+        next_experiments = str(strategy.suggest_experiments(1)).split('\n')[2].split()
+        print(str(next_experiments))
+        single_experiment(next_experiments[1], next_experiments[2], next_experiments[3])
+    data_path = pathlib.Path()
+    # new experiments: data are given by an optimizing strategy
+    strategy = SOBO(domain)
+    for i in range(20):
+        next_experiments = str(strategy.suggest_experiments(1)).split('\n')[2].split()
+        print(str(next_experiments))
+        single_experiment(next_experiments[1], next_experiments[2], next_experiments[3])
+        ds = DataSet.read_csv(data_path / 'experiment.csv')
+        exp = ExperimentalEmulator(model_name='Model', domain=domain, dataset=ds)
+        exp.train(max_epochs=50, cv_fold=1, test_size=0.2)
+
+
--- Detector.py
+++ Detector.py
@@ -1,18 +1,10 @@
 # Getting result from the inline IR
 # Serial number might be needed from Mettler Torendo service
 
-import csv
-
-reader = csv.reader(open('C:/Users/z1445/Desktop/exp.csv', 'r'))
-
-for row in reader:
-    print(row)
-
-print()
-
+from random import random
 
 def detect():
-    return 0
+    return 100 * random()
 
 
 # Ask JW to turn on the inline IR machine and see what the csv file look like.
