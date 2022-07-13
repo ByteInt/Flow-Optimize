8bdb2254b8318e9e58f83ff8c44e8c8eb25baba4
Page <68625666+ByteInt@users.noreply.github.com>
2022-07-13 17:09:55 +0800

    A demo is created for expecting next 2 experiments based on randomly generated experiment results

--- Commandline.py
+++ Commandline.py
@@ -1,5 +1,8 @@
+import math
 import pathlib
 import time
+from random import random
+
 import pkg_resources
 from summit.domain import *
 from summit.benchmarks import SnarBenchmark, ExperimentalEmulator
@@ -13,7 +16,6 @@ import Device
 def initiate():
     global domain, ds, strategy, writer
     # The csv file for experiment data
-    writer = csv.writer(open('experiment.csv', 'w', newline=''))
 
     # STRATEGY the strategy in pysummit
 
@@ -35,23 +37,23 @@ def initiate():
         bounds=[0, 1]
     )
     domain += ContinuousVariable(
-        name='yld',
+        name='conti_2',
         description='5',
+        bounds=[0,1],
+        is_objective=True
+    )
+    domain += ContinuousVariable(
+        name='yld',
+        description='6',
         bounds=[0,100],
         is_objective=True
     )
-
-    row_1 = ['NAME']
-    row_2 = ['TYPE']
-    for variable in domain.variables:
-        print(str(variable.name))
-        row_1.append(str(variable.name))
-        row_2.append('DATA')
-    print(row_1)
-    writer.writerow(row_1)
-    writer.writerow(row_2)
+    print(domain.variables)
+    # Generate title of the csv file
+    generate_title()
     # Test data
-    writer.writerows([[0, 'a', 0, 0], [1, 'b', 0, 0]])
+    generate_data()
+    # The problem is that, the changes of csv file can only be applied when the program is over.
     time.sleep(1)
 
     '''transform = MultitoSingleObjective(
@@ -59,8 +61,36 @@ def initiate():
     )'''
 
     strategy = SOBO(domain)
-    ds = DataSet.read_csv("experiment.csv")
+    data_path = pathlib.Path()
+    ds = DataSet.read_csv(data_path / 'experiment.csv')
 
+
+def generate_title():
+    # global writer, csvfile
+    with open('experiment.csv', 'w', newline='') as csvfile:
+        writer = csv.writer(csvfile)
+        row_1 = ['NAME']
+        row_2 = ['TYPE']
+        for variable in domain.variables:
+            print(str(variable.name))
+            row_1.append(str(variable.name))
+            row_2.append('DATA')
+        print(row_1)
+        writer.writerow(row_1)
+        writer.writerow(row_2)
+
+
+def generate_data():
+    with open('experiment.csv', 'a', newline='') as csvfile:
+        writer = csv.writer(csvfile)
+        for i in range(100):
+            if i<50:
+                cate = 'a'
+            else:
+                cate = 'b'
+            randrow = [i, cate, random(), random(), 100*random()]
+            writer.writerow(randrow)
+        csvfile.close()
 
 if __name__ == '__main__':
     initiate()
@@ -68,4 +98,3 @@ if __name__ == '__main__':
     exp.train(max_epochs=500, cv_fold=10, test_size=0.2)
     next_experiments = strategy.suggest_experiments(2)
     print(str(next_experiments))
-
--- Summit_test.py
+++ Summit_test.py
@@ -1,23 +1,94 @@
-# Import summit
+from summit.benchmarks import ExperimentalEmulator
+from summit.domain import *
+from summit.utils.dataset import DataSet
+import pkg_resources
+import pathlib
+
+from summit import Runner
+from summit.strategies import Random, SOBO, MultitoSingleObjective, TSEMO, LHS
 from summit.benchmarks import SnarBenchmark
-from summit.strategies import NelderMead, MultitoSingleObjective
-from summit.run import Runner
+from summit.utils.dataset import DataSet
+import matplotlib.pyplot as plt
 
-# Instantiate the benchmark
-exp = SnarBenchmark()
+# 创建一个Domain类，便于后面添加变量
+domain = Domain()
 
-# Since the Snar benchmark has two objectives and Nelder-Mead is single objective, we need a multi-to-single objective transform
+# 添加一个列举的变量。例如催化剂，碱，溶剂等等，使用CategorialVariable
+des_1 = "Catalyst type - different ligands"
+domain += CategoricalVariable(
+    name="catalyst",
+    description=des_1,
+    levels=[
+        "P1-L1",
+        "P2-L1",
+        "P1-L2",
+        "P1-L3",
+        "P1-L4",
+        "P1-L5",
+        "P1-L6",
+        "P1-L7",
+    ],
+)
+# 数值变量。温度，反应时间，催化剂载量S/C，体系压力等，使用ContinuousVariable
+des_2 = "Residence time in seconds (s)"
+domain += ContinuousVariable(name="t_res", description=des_2, bounds=[60, 600])
+
+des_3 = "Reactor temperature in degrees Celsius (ºC)"
+domain += ContinuousVariable(
+    name="temperature", description=des_3, bounds=[30, 110]
+)
+
+des_4 = "Catalyst loading in mol%"
+domain += ContinuousVariable(
+    name="catalyst_loading", description=des_4, bounds=[0.5, 2.5]
+)
+# 实验结果。产率，ee值，TON等使用ContinuousVariable, 其中is_objective = True
+# maximize根据情况设置
+des_5 = "Yield"
+domain += ContinuousVariable(
+    name="yld",
+    description=des_5,
+    bounds=[0, 100],
+    is_objective=True,
+    maximize=True,
+)
+
+
+des_6 = (
+    "Turnover number - moles product generated divided by moles catalyst used"
+)
+domain += ContinuousVariable(
+    name="ton",
+    description=des_6,
+    bounds=[0, 200],
+    is_objective=True,
+    maximize=True,
+)
+# 确认实验变量和范围
+print(str(domain))
+
+# 已有实验数据所在的文件夹。实验数据按照要求保存为csv格式
+DATA_PATH = pathlib.Path(pkg_resources.resource_filename("summit", "benchmarks/data"))
+
+# 读取已有实验数据
+ds = DataSet.read_csv(DATA_PATH / "reizman_suzuki_case_1.csv")
+# 根据已有数据训练出模型，“exp”
+exp = ExperimentalEmulator(model_name='your_reizman', domain=domain, dataset=ds)
+# verbose：实时观察
+exp.train(max_epochs=500, cv_fold=10, test_size=0.2, verbose=0)
+
+# 检验训练的模型
+fig, ax = exp.parity_plot(include_test=True)
+
+
+# expression输入参数。有同时优化的参数时，选择Multi to Single
+# 然后输入对多个参数的“取舍”，即多个参数的优化表达式
 transform = MultitoSingleObjective(
-    exp.domain, expression="-sty/1e4+e_factor/100", maximize=False
+    exp.domain, expression="yld+0*ton", maximize=True
 )
-for v in exp.domain.variables:
-    print(v.name)
 
-# Set up the strategy, passing in the optimisation domain and transform
-nm = NelderMead(exp.domain, transform=transform)
+strategy = SOBO(exp.domain, transform = transform)
 
-# Use the runner to run closed loop experiments
-r = Runner(
-    strategy=nm, experiment=exp,max_iterations=50
-)
-# r.run()
+next_experiments = strategy.suggest_experiments(2)
+print(str(next_experiments))
+
